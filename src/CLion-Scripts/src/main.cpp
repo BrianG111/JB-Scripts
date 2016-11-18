6099bcdb5416dd8b0e32521e28e069a9e658cbeb
BrianG111 <brian20082003@live.nl>
2016-11-18 08:25:51 +0100

    Upload 18-11-2016

---
+++ src/CLion-Scripts/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+// main() is where program execution begins
+
+
+int main()
+{
+    cout << "Hello World"; // Prints Hello World
+    return 0;
+}
